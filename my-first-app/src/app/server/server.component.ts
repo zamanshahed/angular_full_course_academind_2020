9be844b7e96600f24373507fe3629aa49a1209da
Shahed <shaheduz.zaman@northsouth.edu>
2020-07-28 06:34:03 +0600

    manually component created and tested.

---
+++ my-first-app/src/app/server/server.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    //setting js objects or metadata that says what to do using this class
+    selector:'app-server',
+    templateUrl:'./server.component.html'
+})
+export class ServerComponent{
+
+}
