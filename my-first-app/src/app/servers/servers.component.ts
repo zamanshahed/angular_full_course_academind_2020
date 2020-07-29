21281f64951ce2c55ce5b760f8d47c0693adf3e4
Shahed <shaheduz.zaman@northsouth.edu>
2020-07-29 21:34:51 +0600

    template: with and without external html file

--- my-first-app/src/app/servers/servers.component.ts
+++ my-first-app/src/app/servers/servers.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
-  templateUrl: './servers.component.html',
+  // templateUrl: './servers.component.html',
+  template: '<app-servers></app-servers>',
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
