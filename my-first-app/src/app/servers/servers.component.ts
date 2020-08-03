ca876683c63aa8a029dc84491ae75b35e9142dac
Shahed <shaheduz.zaman@northsouth.edu>
2020-08-03 13:37:26 +0600

    2.15: Property binding part 1

--- my-first-app/src/app/servers/servers.component.ts
+++ my-first-app/src/app/servers/servers.component.ts
@@ -7,8 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
+  allowNewServer = false;
 
-  constructor() { }
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2500);
+   }
 
   ngOnInit(): void {
   }
