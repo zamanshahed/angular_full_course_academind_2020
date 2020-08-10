fa3b7078d90047430b1e0047d7df2e02f86d889b
Shahed <shaheduz.zaman@northsouth.edu>
2020-08-10 08:38:38 +0600

    minor updates, improvements for assignment3 soln

--- my-first-app/src/app/soln3/soln3.component.ts
+++ my-first-app/src/app/soln3/soln3.component.ts
@@ -15,7 +15,8 @@ export class Soln3Component implements OnInit {
   }
   onToggleDetails(){
     this.showSecret = !this.showSecret;
-    this.log.push(this.log.length+1);
+    // this.log.push(this.log.length+1);
+    this.log.push(new Date());
   }
 
 }
